1c9148f06085390fc5e1a7286ccab95ba95814ba
Jonathan_Falise <falij1@bfh.ch>
2023-04-30 07:49:14 +0000

    implemented safety level

--- AutoMobRobS/src/MyRobotSafetyProperties.cpp
+++ AutoMobRobS/src/MyRobotSafetyProperties.cpp
@@ -4,10 +4,18 @@ MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
     : cs(cs),
     
       slSystemOff("System is offline"),
+      slShuttingDown("System is shutting down"),
+      slSystemStartingUp("System is starting up"),
+      slEmergency("System has detected an Emergency"),
       slSystemOn("System is online"),
+      slMoving("System is Moving"),
 
+      doSystemOff("Shutdown the system"),
+      doSystemStartingUp("Starting up the system"),
+      doEmerency("Emerengy Detected"),
+      doEmerencyResolved("Emerengy cleared"),
       doSystemOn("Startup the system"),
-      doSystemOff("Shutdown the system")
+      doStartMoving("System is starting to move")
 {
     eeros::hal::HAL &hal = eeros::hal::HAL::instance();
 
@@ -24,10 +32,15 @@ MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
     // Add all safety levels to the safety system
     addLevel(slSystemOff);
     addLevel(slSystemOn);
+    addLevel(slSystemStartingUp);
+    addLevel(slEmergency);
+    addLevel(slSystemOn);
+    addLevel(slMoving);
 
     // Add events to individual safety levels
-    slSystemOff.addEvent(doSystemOn, slSystemOn, kPublicEvent);
+    slSystemOff.addEvent(doSystemOn, slSystemStartingUp, kPublicEvent);
     slSystemOn.addEvent(doSystemOff, slSystemOff, kPublicEvent);
+    slSystemStartingUp.addEvent(doStartingUp, kPublicEvent);
 
     // Add events to multiple safety levels
     // addEventToAllLevelsBetween(lowerLevel, upperLevel, event, targetLevel, kPublicEvent/kPrivateEvent);
